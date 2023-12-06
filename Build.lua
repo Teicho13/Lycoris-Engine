c79ff596acee15ec66f7509c434fd018988a48bd
Tycho <t.charko@outlook.com>
2023-12-06 14:22:49 +0100

    Add Project

---
+++ Build.lua
@@ -0,0 +1,17 @@
+-- premake5.lua
+workspace "Lycoris Engine"
+   architecture "x64"
+   configurations { "Debug", "Release", "Dist" }
+   startproject "App"
+
+   -- Workspace-wide build options for MSVC
+   filter "system:windows"
+      buildoptions { "/EHsc", "/Zc:preprocessor", "/Zc:__cplusplus" }
+
+OutputDir = "%{cfg.system}-%{cfg.architecture}/%{cfg.buildcfg}"
+
+group "Core"
+	include "Lycoris-Core/Build-Core.lua"
+group ""
+
+include "Lycoris-App/Build-App.lua"
