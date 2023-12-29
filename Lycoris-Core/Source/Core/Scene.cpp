5da8e9c44e7f8d89af1b79ff0cf41a86d768ff21
Tycho <t.charko@outlook.com>
2023-12-29 15:41:49 +0100

    Simple Transform
    Added a entity with the transform component and printed the result.

--- Lycoris-Core/Source/Core/Scene.cpp
+++ Lycoris-Core/Source/Core/Scene.cpp
@@ -2,11 +2,28 @@
 #include "entt.hpp"
 #include "Components/Transform.h"
 
+#include <iostream>
+
 namespace Lycoris
 {
 	Scene::Scene() 
 	{
-		
+		//Create Entity
+		entt::entity entity = m_Registry.create();
+
+		//Add component to entity
+		m_Registry.emplace<TransformComponent>(entity, glm::mat4(1.0f));
+
+		//Create Entity 2
+		entt::entity entity2 = m_Registry.create();
+		m_Registry.emplace<TransformComponent>(entity2, glm::mat4(1.0f));
+
+		//Get all components with transform component
+		auto view = m_Registry.view<TransformComponent>();
+		for (auto entity : view)
+		{
+			std::cout << "Entity with transform component\n";
+		}
 	}
 
 	Scene::~Scene()
